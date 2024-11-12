6e2dd76ed944cee0073273a851bbc23be9bf80eb
leesua <s2332@e-mirim.hs.kr>
2024-11-12 16:05:04 +0900

    SFML 테스트 및 Deck 클래스 구현중

--- main.cpp
+++ main.cpp
@@ -1,15 +1,44 @@
-﻿#include <iostream>
+﻿#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 class Card {
 public:
-	string suit; //카드 문양
-	string rank; //카드 숫자 또는 영어 값
-	int value; // 카드 점수 값
-	string type; // "number" 또는 "operation", 일반 카드와 사칙연산 카드 구별
+    string suit; //카드 문양
+    string rank; //카드 숫자 또는 영어 값
+    int value; // 카드 점수 값
+    string type; // "number" 또는 "operation", 일반 카드와 사칙연산 카드 구별
 
-	Card(string s, string r, int v, string t) : suit(s), rank(r), value(v), type(t) {} 
+    Card(string s, string r, int v, string t) : suit(s), rank(r), value(v), type(t) {}
 };
 
-void main(void) {
+class Deck {
+public:
 
+private:
+    vector<Card> card;
+};
+
+int main() {
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test Window");
+
+    // 메인 루프
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // 창 닫기 이벤트 처리
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // 화면을 흰색으로 클리어
+        window.clear(sf::Color::White);
+
+        // 창을 갱신
+        window.display();
+    }
+
+	return 0;
 }
